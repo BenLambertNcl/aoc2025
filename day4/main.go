85ae045a55f87dd53d00477181a85d1e959eb350
BenLambertNcl <9324259+BenLambertNcl@users.noreply.github.com>
2025-12-04 08:16:22 +0000

    Day 4 completed

--- day4/main.go
+++ day4/main.go
@@ -8,23 +8,105 @@ import (
 	"strings"
 )
 
-func part1(lines []string) int {
-	return 0
+type positions struct {
+	x, y int
 }
 
-func part2(lines []string) int {
-	return 0
+func checkAdjacentPositions(grid [][]string, x, y int) int {
+	gridHeight := len(grid)
+	gridWidth := len(grid[0])
+
+	count := 0
+
+	allPositions := []positions{
+		{x - 1, y - 1}, // left upper diag
+		{x, y - 1},     // upper
+		{x + 1, y - 1}, // right upper diag
+		{x - 1, y},     // left
+		{x + 1, y},     // right
+		{x - 1, y + 1}, // right lower diag
+		{x, y + 1},     // lower
+		{x + 1, y + 1}, // right lower diag
+	}
+
+	for _, pos := range allPositions {
+		if pos.x >= 0 && pos.x < gridWidth && pos.y >= 0 && pos.y < gridHeight {
+			if grid[pos.y][pos.x] == "@" {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func forEachGridPosition(grid [][]string, fn func(x, y int)) {
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			fn(x, y)
+		}
+	}
+}
+
+func part1(grid [][]string) int {
+	total := 0
+	forEachGridPosition(grid, func(x, y int) {
+		if grid[y][x] == "@" {
+			count := checkAdjacentPositions(grid, x, y)
+			if count < 4 {
+				total++
+			}
+		}
+	})
+
+	return total
+}
+
+func part2(grid [][]string) int {
+	total := 0
+	positionsToReplace := []positions{}
+	currentTotal := -1
+
+	for currentTotal != 0 {
+		currentTotal = 0
+		forEachGridPosition(grid, func(x, y int) {
+			if grid[y][x] == "@" {
+				count := checkAdjacentPositions(grid, x, y)
+				if count < 4 {
+					currentTotal++
+					positionsToReplace = append(positionsToReplace, positions{x, y})
+				}
+			}
+		})
+
+		for _, pos := range positionsToReplace {
+			grid[pos.y][pos.x] = "."
+		}
+
+		total += currentTotal
+	}
+
+	return total
 }
 
 func main() {
-	lines := readInstructions()
-	fmt.Println("Day 1:", part1(lines))
-	fmt.Println("Day 2:", part2(lines))
+	grid := readInstructionsAsGrid()
+	fmt.Println("Day 1:", part1(grid))
+	fmt.Println("Day 2:", part2(grid))
 }
 
 func readInstructions() []string {
 	data, _ := os.ReadFile("input.txt")
 	return strings.Split(string(data), "\n")
+}
+
+func readInstructionsAsGrid() [][]string {
+	data, _ := os.ReadFile("input.txt")
+	lines := strings.Split(string(data), "\r\n")
+	grid := make([][]string, len(lines))
+	for i, line := range lines {
+		grid[i] = strings.Split(line, "")
+	}
+	return grid
 }
 
 func parseIntOrError(num string) int {
--- day4/main_test.go
+++ day4/main_test.go
@@ -1,1 +1,78 @@
 package main
+
+import "testing"
+
+func TestAdjacentPositions(t *testing.T) {
+	grid := [][]string{
+		{"@", "@", "@"},
+		{"@", ".", "@"},
+		{"@", "@", "@"},
+	}
+
+	want := 8
+	got := checkAdjacentPositions(grid, 1, 1)
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestAdjacentPositionsSomeMissing(t *testing.T) {
+	grid := [][]string{
+		{"@", "@", "."},
+		{".", ".", "@"},
+		{"@", ".", "@"},
+	}
+
+	want := 5
+	got := checkAdjacentPositions(grid, 1, 1)
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestAdjacentPositionsAllMissing(t *testing.T) {
+	grid := [][]string{
+		{".", ".", "."},
+		{".", ".", "."},
+		{".", ".", "."},
+	}
+
+	want := 0
+	got := checkAdjacentPositions(grid, 1, 1)
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestAdjacentPositionsDoesntGoOutOfBounds(t *testing.T) {
+	grid := [][]string{
+		{".", ".", "."},
+		{".", "@", "."},
+		{".", ".", "."},
+	}
+
+	want := 1
+	got := checkAdjacentPositions(grid, 0, 0)
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	grid := [][]string{
+		{"@", "@", "@"},
+		{"@", "@", "@"},
+		{"@", "@", "@"},
+	}
+
+	want := 4
+	got := part1(grid)
+
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
